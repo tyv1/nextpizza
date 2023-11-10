d74c377240f0c3250484d3c52ef979115db6309f
Emil <eetaller@gmail.com>
2023-11-10 16:23:25 +0100

    Add error handling for API fetch on menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -14,29 +14,41 @@ async function getData() {
 
 
 export default async function Home() {
-  const data = await getData()
+  let data = []
 
+  try {
+    data = await getData()
+  } catch (error: any) {
+    console.error(error.message)
     return (
       <main className="min-h-screen">
-          <h1 className="text-6xl font-bold text-center">Menu</h1>
-          <div className={styles.list}>
-            {data.map((pizza: any) => (
-              <div className={styles.item} key={pizza.id}>
-                <h2 className={styles.name}>{pizza.name}</h2>
-                <p className={styles.description}>{pizza.description}</p>
-                <p className={styles.toppings}>{pizza.toppings.map(
-                  (topping: any) => topping.name
-                ).join(', ')}
-                </p>
-                <div className={styles.imgWrapper}>
-                  <img src={pizza.image} alt={pizza.name} />
-                </div>
-                <p className={styles.price}>{pizza.price},-</p>
-                <button className={styles.button}>Add to cart</button>
-              </div>
-            ))}
-          </div>
+        <h1 className="text-6xl font-bold text-center">Menu</h1>
+        <h2 className="text-3xl font-bold text-center">Error:</h2>
+        <p className="text-center">{error.message}</p>
       </main>
     )
   }
-  
+
+  return (
+    <main className="min-h-screen">
+      <h1 className="text-6xl font-bold text-center">Menu</h1>
+      <div className={styles.list}>
+        {data.map((pizza: any) => (
+          <div className={styles.item} key={pizza.id}>
+            <h2 className={styles.name}>{pizza.name}</h2>
+            <p className={styles.description}>{pizza.description}</p>
+            <p className={styles.toppings}>{pizza.toppings.map(
+              (topping: any) => topping.name
+            ).join(', ')}
+            </p>
+            <div className={styles.imgWrapper}>
+              <img src={pizza.image} alt={pizza.name} />
+            </div>
+            <p className={styles.price}>{pizza.price},-</p>
+            <button className={styles.button}>Add to cart</button>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
