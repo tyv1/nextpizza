33945d3b3c08631a646be02fdf7cd015277e1364
Emil <eetaller@gmail.com>
2023-11-10 08:26:22 +0100

    Add menu page with list of pizzas

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,8 +1,41 @@
+import styles from './page.module.css'
 
-export default function Home() {
+const URL = 'http://127.0.0.1:8000/api/pizzas' // TODO: replace with real API
+
+async function getData() {
+  const res = await fetch(URL)
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  
+  return res.json()
+}
+
+
+export default async function Home() {
+  const data = await getData()
+
     return (
       <main className="min-h-screen">
-        <p>Menu page</p>
+          <h1 className="text-6xl font-bold text-center">Menu</h1>
+          <div className={styles.list}>
+            {data.map((pizza: any) => (
+              <div className={styles.item} key={pizza.id}>
+                <h2 className={styles.name}>{pizza.name}</h2>
+                <p className={styles.description}>{pizza.description}</p>
+                <p className={styles.toppings}>{pizza.toppings.map(
+                  (topping: any) => topping.name
+                ).join(', ')}
+                </p>
+                <div className={styles.imgWrapper}>
+                  <img src={pizza.image} alt={pizza.name} />
+                </div>
+                <p className={styles.price}>{pizza.price},-</p>
+                <button className={styles.button}>Add to cart</button>
+              </div>
+            ))}
+          </div>
       </main>
     )
   }
